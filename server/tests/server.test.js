4124e4ade9fe24ff1e90265647f5403f485b4979
sat1sandhu <sat1sandhu@yahoo.com>
2017-06-30 17:21:06 +1000

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,36 @@ const request = require('supertest');
 const {app}   = require('./../server');
 const {Todo}  = require('./../models/todo');
 
+// /// Useful for the POST /todos - Remove all todos collection documents
+// /// This was used only to test POST /todos
+// beforeEach( (done) => {
+//     Todo.remove({})
+//       .then ( () => done());
+// });
+
+
+/// When running POST /todos, we remove all todos documents. This is not good for the GET /todos,
+///  which requires documents in the todos collection. So we add some todos documents for the GET /todos
+///  test
+const todos = [{
+    text: "First test todos document"
+},{
+    text: "Second test todos document"
+}];
+
+
+/// Useful for both GET /todos and  POST /todos
 beforeEach( (done) => {
     Todo.remove({})
-      .then ( () => done());
+      .then ( () => {
+          return Todo.insertMany(todos);
+      })
+      .then ( () => {
+          done();
+      })
+      .catch ( (err) => {
+          return done(err);
+      });
 });
 
 describe('POST /todos', () => {
@@ -25,7 +52,8 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find()
+                /// Search the todos collection for document with text =  'Test todo text'
+                Todo.find({text})
                   .then ( (todos) => {
                       expect(todos.length).toBe(1);
                       expect(todos[0].text).toBe(text);
@@ -53,7 +81,7 @@ describe('POST /todos', () => {
 
               Todo.find()
                 .then ( (todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 })
                 .catch ( (err) => {
@@ -63,4 +91,19 @@ describe('POST /todos', () => {
     });
 
 
+});
+
+
+
+describe ( 'GET /todos', () => {
+
+    it ('Should get all todo documents', (done) => {
+          request(app)
+            .get('/todos')
+            .expect(200)
+            .expect ( (res) => {
+                expect(res.body.docs.length).toBe(2);
+            })
+            .end( done);
+    });
 });
