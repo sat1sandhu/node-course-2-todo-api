e8ca47c300366c2f210a72b51654a7668b1055f4
sat1sandhu <sat1sandhu@yahoo.com>
2017-08-22 08:39:31 +1000

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,59 @@
 /// Don't need to load in the db/mongoose.js file; just need the mongoose library
-var mongoose = require('mongoose');
+const mongoose   = require ('mongoose');
+const validator  = require ('validator');
+const jwt        = require ('jsonwebtoken');
+const _          = require ('lodash');
 
-/// Create a Mongoose model for user
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema ({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
-
+        trim: true,
+        unique: true,
+        validate: {
+          validator: validator.isEmail,
+          message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+      var user = this;
+      var access = 'auth';
+      var token  = jwt.sign ({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+      user.tokens.push ({access, token});
+
+      return user.save()
+        .then ( () => {
+            return token;
+        });
+};
+
+/// Create a Mongoose model for user
+var User = mongoose.model('User', UserSchema);
 
 /// Export the model. Else we can't use in other files that need User
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -47,6 +47,23 @@ app.post('/todos', (req, res) => {
     });
 });
 
+/// Create a new User
+app.post ('/users', (req, res) => {
+    var body = _.pick (req.body, ['email', 'password']);
+    var user = new User (body);
+    user.save()
+      .then ( () => {
+            return user.generateAuthToken();
+      })
+      .then ( (token) => {
+            res.header('x-auth',token).send(user);
+      })
+      .catch ( (err) => {
+            res.status(400).send(err);
+      });
+
+});
+
 
 app.get('/todos', (req, res) => {
     Todo.find()
