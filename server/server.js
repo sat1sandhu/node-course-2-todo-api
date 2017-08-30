a13be5cf3a65d9e89e52604c35a6fabb4ee78106
sat1sandhu <sat1sandhu@yahoo.com>
2017-08-30 17:35:23 +1000

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -101,6 +101,14 @@ app.delete('/todos/:id', (req, res) => {
         });
 });
 
+app.delete ('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken (req.token)
+      .then ( () => {
+          res.status(200).send();
+      }, () => {
+          res.status(400).send();
+      });
+});
 
 /// Create a patch route. Patch is what we use to update a resource
 app.patch('/todos/:id', (req, res) => {
