b6cdbeee2b995485e97e41604322f6c6dd97fe16
sat1sandhu <sat1sandhu@yahoo.com>
2017-08-31 15:19:43 +1000

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -407,8 +407,29 @@ describe ('POST /users/login', () => {
                 .catch ( (err) => done(err));
           });
   });
+});
 
+describe ('DELETE /users/me/token', () => {
+    it ('Should remove auth token on logout', (done) => {
+        //console.log ("token = ", users[0].tokens[0].token);
+        
+        request (app)
+          .delete ('/users/me/token')
+          .set ('x-auth', users[0].tokens[0].token)
+          .expect (200)
+          .end ( (err, res) => {
+                if (err) {
+                    return done(err);
+                }
 
+                User.findById (users[0]._id)
+                  .then ( (user) => {
+                      expect (user.tokens.length).toBe(0);
+                      done();
+                  })
+                  .catch ( (err) => done(err) );
+              });
+    });
 });
 
 //});
